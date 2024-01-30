66bc3f98c9c7ec9fbcbadbbbe2e30526341d7fb4
Joe Pea <joe@trusktr.io>
2024-01-30 00:46:04 -0800

    define the template in the user class body so that it picks up all scope as expected, update example to show usage of module variables and private fields within the template

--- elements/foo.js
+++ elements/foo.js
@@ -1,1 +1,2 @@
+// This var is imported inside of an element module and the value is used in an custom element's template.
 export const foo = 'boo!'
--- very-rough-ponyfill.js
+++ very-rough-ponyfill.js
@@ -1,11 +1,7 @@
 // @ts-check
 
-// @ts-expect-error we're using this import inside the eval() below.
-import {html} from 'pota/src/renderer/@main.js'
-
 const moduleSpecifier = /(?:import|from)\s*['"](.+?)['"]/gm // regexr.com/7r66e
-
-let bypassAttachShadowRestriction = false
+const classHeader = /export\s*default\s*class\s*([a-zA-Z]*)\s*(?:extends\s*([a-zA-Z]+))?\s*{/
 
 export async function import_concept(importSpecifier, importAttributes) {
 	const htmlCode = await fetch(importSpecifier).then(r => r.text())
@@ -25,95 +21,120 @@ export async function import_concept(importSpecifier, importAttributes) {
 
 		// if no name, no automatic definition (TODO derive from class name as fallback?)
 		if (elementName) {
-			const exported = def.getAttribute('export')
+			const exported = def.getAttribute('export') // TODO export classes marked with export
 			const shadowMode = /** @type {'open' | 'closed'} */ (def.getAttribute('shadowmode')) ?? 'open'
 			const scripts = def.querySelectorAll('script')
 			const scriptEl = scripts[0]
 
 			for (const script of Array.from(scripts)) script.remove()
 
-			let jsCode = scriptEl?.textContent
+			let userCode = scriptEl?.textContent
 
-			let module = {}
+			// If the user did not provide a script, define an implicit class.
+			if (!userCode) userCode = `export default class extends HTMLElement {}`
 
-			if (jsCode) {
-				jsCode = implementImportMetaUrl(jsCode, htmlUrl)
-				jsCode = implementRelativeSpecifiers(jsCode, htmlUrl)
+			// If there's a user script but no exported class, imply the default class.
+			if (!userCode.match(classHeader)) userCode += `; export default class extends HTMLElement {}`
 
-				const scriptUrl = URL.createObjectURL(new Blob([jsCode], {type: 'application/javascript'}))
-				module = await import(scriptUrl)
-				URL.revokeObjectURL(scriptUrl)
-			}
+			userCode = implementImportMetaUrl(userCode, htmlUrl)
+			userCode = implementRelativeSpecifiers(userCode, htmlUrl)
+
+			const header = userCode.match(classHeader)[0]
+
+			// Place the template inside the user class definition so it can access private fields.
+			userCode = userCode.replace(
+				header,
+				/*js*/ `const __TEMPLATE__ = Symbol()
+                ${header}
+                [__TEMPLATE__] = () => html\`${def.innerHTML}\``,
+			)
+
+			const newHeader = header.replace('export default', 'const __DEFAULT_EXPORT__ =')
+			userCode = userCode.replace(header, newHeader)
+
+			userCode += /*js*/ `
+                import {html} from 'pota/src/renderer/@main.js'
+
+                const Class = __DEFAULT_EXPORT__
+
+                /** @param {string} str */
+                function toCamelCase(str) {
+                    return str.replace(/-[a-zA-Z]/g, s => s[1].toUpperCase())
+                }
+
+                let bypassAttachShadowRestriction = false
+
+                // TODO use scoped registry for the non-exported element
+                // (internal-el) so it remains internal to some-el instead of
+                // globally defined.
+                // Perhaps try with the polyfill: https://github.com/webcomponents/polyfills/blob/master/packages/scoped-custom-element-registry
+                // const scopedElements = new CustomElementRegistry()
+
+                // In the native implementation, we would not extend from the
+                // author's class, the definition would be the author's class
+                // directly. We just do this as a way to attach a shadow root to
+                // show a rough concept. In the native implementation, the
+                // element instance will already have a shadow root by the time
+                // the author's class runs and will be available via
+                // ElementInternals (similar to DSD), and this roughly
+                // approximates that by forcing attachShadow() throw an error
+                // when we have not allowed it to be callable using
+                // bypassAttachShadowRestriction.
+                export default class extends Class {
+                    static name = toCamelCase("${elementName}")
+
+                    /** @type {ElementInternals} */
+                    #internals
+
+                    constructor() {
+                        super()
+                        if (!this.#internals) this.attachInternals()
+                    }
+
+                    /**
+                     * @param {ShadowRootInit} init
+                     * @returns {ShadowRoot}
+                     */
+                    attachShadow(init) {
+                        // Element authors cannot call attachShadow for elements
+                        // defined by element module, but we need to call it to
+                        // create the root, so we allow only this ponyfill to
+                        // call it.
+                        if (bypassAttachShadowRestriction) return super.attachShadow(init)
+                        throw new TypeError('attachShadow() cannot be called on elements imported from element modules. Use attachInternals().shadowRoot instead.')
+                    }
+
+                    attachInternals() {
+                        this.#internals = super.attachInternals()
+
+                        // In the current concept design, it should be impossible for the CE author to use attachShadow, the
+                        // root should pre-exist from based on the HTML template that is wrapping the user's <script>.
+                        if (this.#internals.shadowRoot) throw new Error('No handling of pre-existing ShadowRoot for now.')
+
+                        bypassAttachShadowRestriction = true
+                        const root = this.attachShadow({mode: "${shadowMode}" /*, todo customElements: scopedElements */})
+                        bypassAttachShadowRestriction = false
+
+                        const dom = this[__TEMPLATE__]()
+                        root.append(...[dom].flat(Infinity))
+
+                        return this.#internals
+                    }
+                }
+            `
+
+			const userCodeUrl = URL.createObjectURL(new Blob([userCode], {type: 'application/javascript'}))
+
+			const module = await import(userCodeUrl)
+
+			URL.revokeObjectURL(userCodeUrl)
 
 			/** @type {typeof HTMLElement | undefined} */
 			const Class = module.default
 
-			// TODO use scoped registry for the non-exported element (internal-el) so it remains internal to some-el.
-			// Perhaps try with the polyfill: https://github.com/webcomponents/polyfills/blob/master/packages/scoped-custom-element-registry
-			// const scopedElements = new CustomElementRegistry()
-
-			// In the native implementation, we would not extend from the
-			// author's class, the definition would be the author's class
-			// directly. We just do this as a way to attach a shadow root to
-			// show a rough concept. This is not accurate: The element will
-			// already have a shadow root by the time the author's class
-			// runs (like DSD), but in this rough concept, `this.shadowRoot`
-			// in the author's constructor will be `null`, and if they call
-			// `attachShadow`, the method will not error as it would if the
-			// root already pre-existed.
-			const Wrapper = class extends (Class ?? HTMLElement) {
-				static name = toCamelCase(elementName)
-
-				/** @type {ElementInternals} */
-				#internals
-
-				constructor() {
-					super()
-					if (!this.#internals) this.attachInternals()
-				}
-
-				attachInternals() {
-					this.#internals = super.attachInternals()
-
-					// In the real implementation, it will be impossible for
-					// the shadow root to already exist.
-					if (this.#internals.shadowRoot) throw new Error('Just a concept!')
-
-					bypassAttachShadowRestriction = true
-					const root = this.attachShadow({mode: shadowMode /*, todo customElements: scopedElements */})
-					bypassAttachShadowRestriction = false
-
-					const render = eval(`() => html\`${def.innerHTML}\``)
-					root.append(...[render()].flat(Infinity))
-
-					return this.#internals
-				}
-
-				/**
-				 * @param {ShadowRootInit} init
-				 * @returns {ShadowRoot}
-				 */
-				attachShadow(init) {
-					// Element authors cannot call attachShadow for elements
-					// defined by element module, but we need to call it to
-					// create the root, so we allow only this ponyfill to
-					// call it.
-					if (bypassAttachShadowRestriction) return super.attachShadow(init)
-
-					throw new TypeError(
-						'attachShadow() cannot be called on elements imported from element modules. Use attachInternals().shadowRoot instead.',
-					)
-				}
-			}
-
-			customElements.define(elementName, Wrapper)
+			customElements.define(elementName, Class)
 		}
 	}
-}
-
-/** @param {string} str */
-function toCamelCase(str) {
-	return str.replace(/-[a-zA-Z]/g, s => s[1].toUpperCase())
 }
 
 /**
